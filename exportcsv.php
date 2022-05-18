f20748ba916d11b7e0b88b0282ae1a01f24490af
arindamkabir <51151800+arindamkabir@users.noreply.github.com>
2022-05-18 14:42:40 +0600

    Added assign wallet.

--- exportcsv.php
+++ exportcsv.php
@@ -16,9 +16,23 @@ if (($open = fopen("/var/www/v/8.2dev/tools/export.csv", "r")) !== FALSE) {
         // check if email exists
         $check_email = vQ1("select exists (select 1 from tribes where email='" . $data[4] . "');");
 
+        // if !token assign token = id
+        // type = user (ticket, asset, user)
+        // chiefs (json of uids)
         if ($check_email['exists'] == 'f') {
-            $Q = vQ("insert into tribes_test1 (id,name,email,phone) values ('" . uniqid() . "','" . $data[2] . " " . $data[3] . "'," . $data[4] . ",'" . $data[16] . "');");
-        };
+            $tribe = vGUID();
+            $Q = vQ("insert into tribes_test1 (id,token,name,type,email,phone,chiefs) values ('" . $tribe . "','" . $data[2] . " " . $data[3] . "'," . $data[4] . ",'" . $data[16] . "');");
+
+            $sid = vGUID();
+            // create session
+            $Q2 = vQ("insert into sessions (sid, tribe, time) values ('" . $sid . "', '" . $tribe . "', '" . now() . "');");
+
+            // curl to assign wallet
+
+            $meta_tags = get_meta_tags("https://www.seekmintnfts.com/v1/api/assignWallet/?chain=MATIC&_sid=" . $sid);
+        } else {
+            // check the wallet for
+        }
 
 
         vDBClose();
